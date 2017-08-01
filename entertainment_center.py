51fb104ea21376a1c6fe7ec1ab2bfabcedec931e
meowtsai <shihfan.tsai@gmail.com>
2017-08-01 09:54:36 +0800

    Formatting issues fixed per pep8online check results

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,12 +3,16 @@ import fresh_tomatoes
 import requests
 
 
-
-
 # fetch movies data from TMDB server, return a list of movies
 def get_tmdb_movies():
-    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key=cb6f6a2667af1c852790468594b68e7e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2017")
-    if (response.status_code==200):
+    response = requests.get("https://api.themoviedb.org/3/discover/movie"
+                            "?api_key=cb6f6a2667af1c852790468594b68e7e"
+                            "&language=en-US"
+                            "&sort_by=popularity.desc"
+                            "&include_adult=false"
+                            "&include_video=false&page=1"
+                            "&primary_release_year=2017")
+    if (response.status_code == 200):
         data = response.json()
         tmdb_movies = data["results"]
         return tmdb_movies
@@ -16,16 +20,19 @@ def get_tmdb_movies():
         print "movie api request failed!" + str(response.status_code)
         return []
 
+
 # fetch vedio data from TMDB server, return the first YouTube URL string.
 def get_videourl_by_movie(movie_id):
-    videos_response = requests.get("https://api.themoviedb.org/3/movie/" + str(movie_id) + "/videos?api_key=cb6f6a2667af1c852790468594b68e7e&language=en-US")
-    if (videos_response.status_code==200):
+    videos_response = requests.get("https://api.themoviedb.org/3/movie/" +
+                                   str(movie_id) + "/videos?"
+                                   "api_key=cb6f6a2667af1c852790468594b68e7e"
+                                   "&language=en-US")
+    if (videos_response.status_code == 200):
         videos_data = videos_response.json()
-        
         tmdb_video_data = videos_data["results"]
         for video in tmdb_video_data:
-            video_url="";
-            if (video["site"]=="YouTube"):
+            video_url = ""
+            if (video["site"] == "YouTube"):
                 video_url = "https://www.youtube.com/watch?v=" + video["key"]
                 break
         return video_url
@@ -33,21 +40,18 @@ def get_videourl_by_movie(movie_id):
         print "video api request failed!" + str(videos_response.status_code)
         return ""
 
-    
-
-movies =[]
+movies = []
 tmdb_movies = get_tmdb_movies()
 
-# Make all the movies in individul media.Movie() class object, store in an Array, pass the array to fresh_tomatoes.
+# Make all the movies in individul media.Movie() class object,
+# store in an Array, pass the array to fresh_tomatoes.
 for movie in tmdb_movies:
-    video_url=get_videourl_by_movie(movie["id"])
+    video_url = get_videourl_by_movie(movie["id"])
     temp_movie_object = media.Movie(movie["title"],
                                     movie["overview"],
-                                    "https://image.tmdb.org/t/p/w185/" + movie["poster_path"],
+                                    "https://image.tmdb.org/t/p/w185/" +
+                                    movie["poster_path"],
                                     video_url)
     movies.append(temp_movie_object)
-    
-
 
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,15 +1,14 @@
 import webbrowser
 
+
 class Movie():
-    """A movie class contains movie titles, box art, poster images, and movie trailer URLs.
+    """A movie class contains movie titles, box art,
+    poster images, and movie trailer URLs.
     Note this class should work well with fresh_tomatoes.py."""
 
-
-    
-    def __init__(self,title,movie_storyline,poster_image_url,trailer_youtube_url):
+    def __init__(self, title, movie_storyline,
+                 poster_image_url, trailer_youtube_url):
         self.title = title
         self.movie_storyline = movie_storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-
